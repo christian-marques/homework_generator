6e3d45ecf2f003f985d950a53bb8e240a7207b29
Christian Marques <145080782+christian-marques@users.noreply.github.com>
2024-10-22 00:10:07 -0300

    Update app.js
    Modularização do código para facilitar as demais implementações

--- src/app.js
+++ src/app.js
@@ -3,31 +3,39 @@ const Docxtemplater = require('docxtemplater');
 const fs = require('fs');
 const path = require('path');
 
-// Carregar o arquivo .docx como um template
-const content = fs.readFileSync(path.resolve(__dirname, '../files/template.docx'), 'binary');
+// Dada as informações o template é usado para gerar o output
+function generate_file(_template_path, _output_path, _header_info, _message){
 
-const zip = new PizZip(content);
-
-const doc = new Docxtemplater(zip);
-
-// Substituir as tags no template por valores dinâmicos
-doc.setData({
-    name: 'Christian Marques',
-    year: '1º',
-    class: 'Matemática',
-    date: '21/10/2024',
-    text: 'Enunciado 1: Quanto é 1 + 1?'
-});
-
-try {
-    // Renderizar o documento com os dados
-    doc.render();
-} catch (error) {
-    console.error('Erro ao renderizar o documento', error);
+    // Carregar o arquivo .docx como um template
+    const content = fs.readFileSync(path.resolve(__dirname, _template_path), 'binary');
+    
+    const zip = new PizZip(content);
+    
+    const doc = new Docxtemplater(zip);
+    
+    // Substituir as tags no template por valores dinâmicos
+    doc.setData({
+        name: _header_info[0],
+        year: _header_info[1],
+        class: _header_info[2],
+        date: _header_info[3],
+        text: _message
+    });
+    
+    try {
+        // Renderizar o documento com os dados
+        doc.render();
+    } catch (error) {
+        console.error('Erro ao renderizar o documento', error);
+    }
+    
+    // Gerar o arquivo .docx modificado
+    const buffer = doc.getZip().generate({ type: 'nodebuffer' });
+    
+    // Salvar o arquivo
+    fs.writeFileSync(path.resolve(__dirname, _output_path), buffer);
 }
 
-// Gerar o arquivo .docx modificado
-const buffer = doc.getZip().generate({ type: 'nodebuffer' });
 
-// Salvar o arquivo
-fs.writeFileSync(path.resolve(__dirname, '../files/output.docx'), buffer);
+
+generate_file('../files/template.docx', '../files/output.docx', ['Christian Marques', '1º', 'Matemática', '21/10/2024'], 'Enunciado 1: Quanto é 14 * 7?')
