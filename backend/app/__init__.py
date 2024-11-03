638726cb65905ec9a6a856265e366d9e95ee5836
Christian Marques <145080782+christian-marques@users.noreply.github.com>
2024-11-03 00:23:07 -0300

    Alteração para integração com o Render

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,10 +5,8 @@ from .src.routes import word_bp
 def create_app():
     app = Flask(__name__)
 
-    # Habilitar o CORS para o aplicativo Flask
-    CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:8080",
-                                             "http://10.242.242.10:8080",
-                                             "http://192.168.0.120:8080"]}})
+    # Habilitar o CORS para todas as origens
+    CORS(app)  # Permitir todas as origens (para testes no Render)
 
     # Registra o blueprint para os endpoints de geração de Word
     app.register_blueprint(word_bp)
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,9 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Obter a porta a partir da variável de ambiente 'PORT', com fallback para a porta 4000
+    port = int(os.environ.get('PORT', 4000))
+    app.run(host='0.0.0.0', port=port, debug=True)
