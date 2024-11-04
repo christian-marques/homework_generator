24415ed48cd0c22d19e16ea3f36e15139dd27ad9
Christian Marques <145080782+christian-marques@users.noreply.github.com>
2024-11-03 23:17:55 -0300

    Update __init__.py
    Para o render aceitar as requisições do vercel

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -5,8 +5,8 @@ from .src.routes import word_bp
 def create_app():
     app = Flask(__name__)
 
-    # Habilitar o CORS para todas as origens
-    CORS(app)  # Permitir todas as origens (para testes no Render)
+    # Habilitar o CORS para permitir o domínio do Vercel
+    CORS(app, resources={r"/*": {"origins": ["https://homework-generator-azure.vercel.app"]}})
 
     # Registra o blueprint para os endpoints de geração de Word
     app.register_blueprint(word_bp)
